6a97ba90081a6d21262635bd4cbe5d7134720a81
finboy97 <93912397+finboy97@users.noreply.github.com>
2023-08-01 11:42:47 +0100

    Added types to script output main.py

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ import csv
 
 reader = csv.reader
 path_str = input("Input path: ")
-key_name= input("Enter key name: ")
 with open(f"{path_str}", "r") as this_csv:
     reader=csv.reader(this_csv)
     header_row=reader.__next__()
     name_header_list = list()
     for element in header_row:
-        new_tuple = {f"{key_name}": element}
-        name_header_list.append(new_tuple)
+        new_dict = {"Name": element, "Type": "STRING}
+        name_header_list.append(new_dict)
     print(json.dumps(name_header_list, indent=4))
